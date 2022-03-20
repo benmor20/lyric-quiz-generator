ceb0f92143a569f7eee7d360c0887487c40a82f3
benmor20 <benmor2020@gmail.com>
2022-03-19 20:22:44 -0400

    Update gitignore to ignore output

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ def to_rel_file(path):
 
 def main():
     playlist_id = ''
-    answer_key = generate_quiz(20, album='Hamilton (Original Broadway Cast Recording)', limit=60)
+    answer_key = generate_quiz(5, album='Hamilton (Original Broadway Cast Recording)', limit=60)
     print(answer_key)
     with open(to_rel_file('lyric_test.txt'), 'w', encoding='utf-8') as test_file:
         with open(to_rel_file('lyric_test_answers.txt'), 'w', encoding='utf-8') as answer_file:
